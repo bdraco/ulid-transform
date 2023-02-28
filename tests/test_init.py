30dff76e4c77e7d1d66d805ae068467c6e050e8c
J. Nick Koston <nick@koston.org>
2023-02-28 13:04:01 -0600

    coverage

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,11 +1,16 @@
 import pytest
 
-from ulid_transform import ulid, ulid_to_bytes
+from ulid_transform import ulid, ulid_hex, ulid_to_bytes
 
 
 def test_ulid():
     ulid_str = ulid()
     assert len(ulid_str) == 26
+
+
+def test_ulid_hex():
+    ulid_str = ulid_hex()
+    assert len(ulid_str) == 32
 
 
 def test_ulid_to_bytes():
