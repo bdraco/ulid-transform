154bf0c8d02591508b87c2c154ba877da2aa8f97
J. Nick Koston <nick@koston.org>
2023-02-28 12:57:57 -0600

    feat: enable cython builds (#3)

--- src/ulid_transform/__init__.py
+++ src/ulid_transform/__init__.py
@@ -3,6 +3,6 @@ __version__ = "0.1.0"
 # Fast ulid is used for generation
 from fast_ulid import ulid
 
-from .convert import decode_ulid
+from .convert import ulid_to_bytes
 
-__all__ = ["ulid", "decode_ulid"]
+__all__ = ["ulid", "ulid_to_bytes"]
