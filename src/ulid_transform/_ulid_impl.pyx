b975222ac9b3508fbe1a164cfeb2b1cae9e7389d
J. Nick Koston <nick@koston.org>
2023-02-28 15:32:21 -0600

    fix: more

--- src/ulid_transform/_ulid_impl.pyx
+++ src/ulid_transform/_ulid_impl.pyx
@@ -3,15 +3,19 @@ from libcpp.string cimport string
 
 
 cdef extern from "ulid_wrapper.h":
-    string _gen_ulid()
-    string _gen_ulid_at_time(double timestamp)
+    string _cpp_ulid()
+    string _cpp_ulid_at_time(double timestamp)
+    bytes _cpp_ulid_to_bytes(string ulid)
 
 
 def _ulid_now() -> str:
-    ulid_bytes = _gen_ulid()
+    ulid_bytes = _cpp_ulid()
     return ulid_bytes.decode("utf-8")
 
 
 def _ulid_at_time(time: float) -> str:
-    ulid_bytes = _gen_ulid_at_time(time)
+    ulid_bytes = _cpp_ulid_at_time(time)
     return ulid_bytes.decode("utf-8")
+
+def _ulid_to_bytes(ulid: str) -> bytes:
+    return _cpp_ulid_to_bytes(ulid)
--- src/ulid_transform/ulid_wrapper.cpp
+++ src/ulid_transform/ulid_wrapper.cpp
@@ -5,16 +5,21 @@
 using namespace std;
 
 
-std::string _gen_ulid() {
+std::string _cpp_ulid() {
     ulid::ULID ulid;
     ulid::EncodeNowRand(ulid);
     return ulid::Marshal(ulid);
 }
 
-std::string _gen_ulid_at_time(double timestamp) {
+std::string _cpp_ulid_at_time(double timestamp) {
     const auto encoded_time = chrono::system_clock::to_time_t(chrono::system_clock::time_point(chrono::duration_cast<chrono::seconds>(chrono::duration<double>(timestamp))));
     ulid::ULID ulid;
     ulid::EncodeTime(encoded_time, ulid);
     ulid::EncodeEntropyRand(ulid);
     return ulid::Marshal(ulid);
+}
+
+std::vector<uint8_t> _cpp_ulid_to_bytes(const char * ulid_string) {
+    ulid::ULID ulid = ulid::Unmarshal(ulid_string);
+    return ulid::MarshalBinary(ulid);
 }
