c8f7dd790f987ca4310dd155a78fff263adf0cfc
J. Nick Koston <nick@koston.org>
2023-03-13 12:25:00 -1000

    fix: ulid_now on 32bit (#10)

--- src/ulid_transform/_ulid_impl.pyx
+++ src/ulid_transform/_ulid_impl.pyx
@@ -1,20 +1,21 @@
 # distutils: language = c++
 from libcpp.string cimport string
 
+from time import time
+
 import cython
 
 
 cdef extern from "ulid_wrapper.h":
-    string _cpp_ulid()
     string _cpp_ulid_at_time(double timestamp)
     string _cpp_ulid_to_bytes(string ulid)
 
 
 def _ulid_now() -> str:
-    return _cpp_ulid().decode("ascii")
+    return _cpp_ulid_at_time(time()).decode("ascii")
 
-def _ulid_at_time(time: float) -> str:
-    return _cpp_ulid_at_time(time).decode("ascii")
+def _ulid_at_time(_time: float) -> str:
+    return _cpp_ulid_at_time(_time).decode("ascii")
 
 def _ulid_to_bytes(ulid_str: str) -> bytes:
     if len(ulid_str) != 26:
--- src/ulid_transform/ulid_wrapper.h
+++ src/ulid_transform/ulid_wrapper.h
@@ -3,7 +3,6 @@
 #ifndef ULID_WRAPPER_H
 #define ULID_WRAPPER_H
 
-std::string _cpp_ulid();
 std::string _cpp_ulid_at_time(double timestamp);
 std::string _cpp_ulid_to_bytes(std::string ulid_string);
 
