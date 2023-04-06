79e41ea990b0a82e8be6d615f3db2a70f1a01574
Bas Nijholt <basnijholt@gmail.com>
2023-04-06 15:07:47 -0700

    Function already imported

--- tests/test_init.py
+++ tests/test_init.py
@@ -366,6 +366,6 @@ def _ulid_timestamp(ulid: str) -> int:
 
 def test_non_uppercase_b32_data():
     assert (
-        ulid_transform.ulid_to_bytes("not_uppercase_b32_data_:::")
+        ulid_to_bytes("not_uppercase_b32_data_:::")
         == b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\x8c\xff\xff\xff\xff\xff\xff"
     )
